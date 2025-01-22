f8e6e7309c0286209637b77f26d3fce69ca096b1
lim-Ngsw <ancelotty@gmail.com>
2025-01-22 22:57:51 +0900

    コピーライト表記

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -12,7 +12,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
       <main className="flex-1">{children}</main>
       <footer className="border-t py-2">
         <div className="flex flex-col items-center justify-center text-sm space-y-5">
-          <div>©FullStackChannel. ALL Rights Reserved.</div>
+          <div>©2025 Lim. ALL Rights Reserved.</div>
         </div>
       </footer>
     </div>
